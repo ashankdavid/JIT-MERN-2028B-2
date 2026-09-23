c68729009f86bfc157ae7dda3ae0c6d2d037beb8
ashankdavid <ashank2507david@gmail.com>
2026-09-23 10:25:25 +0530

    Delete Feature Completed

--- src/App.jsx
+++ src/App.jsx
@@ -28,11 +28,17 @@ function App() {
     }
 
     function handleDeleteEvent(eventId) {
-        const updatedEvents = events.filter(function (event) {
-            return event.id !== eventId;
+        fetch(`http://localhost:5000/api/events/${eventId}`, {
+            method: "DELETE"
+        }).then((response)=>response.json())
+        .then((data)=>{
+            console.log(data);
+            fetch("http://localhost:5000/api/events")
+            .then((response)=>response.json())
+            .then((data)=>{
+                setEvents(data);
+            });
         });
-
-        setEvents(updatedEvents);
     }
 
     return (
